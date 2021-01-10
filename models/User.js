d71d40992c1d8902539eeb3e8ee53580ee27b7a2
jess-smith49 <js.jessicasmith774@gmail.com>
2021-01-10 17:21:30 -0600

    files and User model

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const {Schema, model} = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: true
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: () => Promise.resolve(false),
+                message: 'Please Enter a Valid e-mail address'
+            }
+        },
+
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thoughts'
+            }
+        ],
+
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Friends'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+
+UserSchema.virtual('friendCount').get(function(){
+    return this.friends.reduce(
+        //have to fill in this virtual
+    )
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
